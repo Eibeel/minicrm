d14b3888502ae8f194a0b002c5dc0b5d4ae6e8c9
Eibeel <abelguardop@gmail.com>
2023-06-02 16:08:17 -0500

    refactor: data entry types

--- src/components/FormAddContact.tsx
+++ src/components/FormAddContact.tsx
@@ -1,5 +1,13 @@
 import { useEffect } from 'react'
-import { Box, Button, Select, Text, VStack, forwardRef } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Select,
+  Text,
+  VStack,
+  forwardRef,
+  useToast
+} from '@chakra-ui/react'
 import DatePicker from 'react-datepicker'
 import { AddUser } from 'iconoir-react'
 import useForm from '../hooks/useForm'
@@ -13,6 +21,7 @@ const FormAddContact = (
   const addContact = useContactsStore(state => state.addContact)
   const updateContact = useContactsStore(state => state.updateContact)
   const contact = useContactsStore(state => state.contacts.find(contact => contact.id === contactId))
+  const toast = useToast()
 
   const { formValues, handleFormChange, handleDateChange, handleSetForm, handleResetForm } = useForm({
     name: '',
@@ -36,9 +45,24 @@ const FormAddContact = (
     }
   }, [contactId])
 
-  const handleAddContact = () => {
-    if (formValues.name.trim() === '') {
-      console.log('rellene todos los campos')
+  const handleAddContact = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (
+      formValues.name.trim() === '' ||
+      formValues.lastName.trim() === '' ||
+      formValues.address.trim() === '' ||
+      formValues.email.trim() === '' ||
+      formValues.cel.trim() === ''
+    ) {
+      toast({
+        position: 'bottom-left',
+        title: 'Algunos campos estan vacíos.',
+        description: 'Por favor, rellene todos los campos.',
+        status: 'error',
+        duration: 2000,
+        variant: 'subtle',
+        isClosable: true
+      })
       return
     }
 
@@ -77,95 +101,99 @@ const FormAddContact = (
   return (
     <Box px={6} pt={6}>
       <Text as='b' fontSize={18} color='#2c6cb0'>Nuevo contacto</Text>
-      <VStack py={4} spacing={4}>
-        <InputForm
-          placeholder='Nombres'
-          name='name'
-          value={formValues.name}
-          onChange={handleFormChange}
-          type='text'
-        />
-        <InputForm
-          placeholder='Apellidos'
-          name='lastName'
-          value={formValues.lastName}
-          onChange={handleFormChange}
-          type='text'
-        />
-        <InputForm
-          placeholder='E-mail'
-          name='email'
-          value={formValues.email}
-          onChange={handleFormChange}
-          type='email'
-        />
-        <InputForm
-          placeholder='Cel'
-          name='cel'
-          value={formValues.cel}
-          onChange={handleFormChange}
-          type='cel'
-        />
-        <DatePicker
-          selected={formValues.birthday}
-          onChange={handleDateChange}
-          customInput={<CustomDatePicker />}
-          maxDate={today}
-        />
-        <InputForm
-          placeholder='Dirección'
-          name='address'
-          value={formValues.address}
-          onChange={handleFormChange}
-          type='text'
-        />
-        <Select
-          bgColor='whiteAlpha.700'
-          placeholder='Tipo de contacto'
-          name='type'
-          value={formValues.typeContact}
-          onChange={handleFormChange}
-        >
-          <option value='cliente'>Cliente</option>
-          <option value='option2'>Option 2</option>
-          <option value='option3'>Option 3</option>
-        </Select>
-        <Select
-          bgColor='whiteAlpha.700'
-          placeholder='Origen'
-          name='origen'
-          value={formValues.origen}
-          onChange={handleFormChange}
-        >
-          <option value='landing'>Landing page</option>
-          <option value='option2'>Option 2</option>
-          <option value='option3'>Option 3</option>
-        </Select>
-      </VStack>
-      {
-        contact !== undefined
-          ? (
-            <Button
-              my={6}
-              w='100%'
-              colorScheme='teal'
-              onClick={handleUpdatedContact}
-            >
-              Actualizar
-            </Button>
-          )
-          : (
-            <Button
-              my={6}
-              w='100%'
-              colorScheme='teal'
-              rightIcon={<AddUser />}
-              onClick={handleAddContact}
-            >
-              Agregar
-            </Button>
-          )
-      }
+      <form onSubmit={handleAddContact}>
+        <VStack py={4} spacing={4}>
+
+          <InputForm
+            placeholder='Nombres'
+            name='name'
+            value={formValues.name}
+            onChange={handleFormChange}
+            type='text'
+          />
+          <InputForm
+            placeholder='Apellidos'
+            name='lastName'
+            value={formValues.lastName}
+            onChange={handleFormChange}
+            type='text'
+          />
+          <InputForm
+            placeholder='E-mail'
+            name='email'
+            value={formValues.email}
+            onChange={handleFormChange}
+            type='email'
+          />
+          <InputForm
+            placeholder='Cel'
+            name='cel'
+            value={formValues.cel}
+            onChange={handleFormChange}
+            type='number'
+          />
+          <DatePicker
+            selected={formValues.birthday}
+            onChange={handleDateChange}
+            customInput={<CustomDatePicker />}
+            maxDate={today}
+          />
+          <InputForm
+            placeholder='Dirección'
+            name='address'
+            value={formValues.address}
+            onChange={handleFormChange}
+            type='text'
+          />
+          <Select
+            bgColor='whiteAlpha.700'
+            placeholder='Tipo de contacto'
+            name='type'
+            value={formValues.typeContact}
+            onChange={handleFormChange}
+          >
+            <option value='cliente'>Cliente</option>
+            <option value='option2'>Option 2</option>
+            <option value='option3'>Option 3</option>
+          </Select>
+          <Select
+            bgColor='whiteAlpha.700'
+            placeholder='Origen'
+            name='origen'
+            value={formValues.origen}
+            onChange={handleFormChange}
+          >
+            <option value='landing'>Landing page</option>
+            <option value='option2'>Option 2</option>
+            <option value='option3'>Option 3</option>
+          </Select>
+        </VStack>
+        {
+          contact !== undefined
+            ? (
+              <Button
+                my={6}
+                w='100%'
+                colorScheme='teal'
+                onClick={handleUpdatedContact}
+                type='submit'
+              >
+                Actualizar
+              </Button>
+            )
+            : (
+              <Button
+                my={6}
+                w='100%'
+                colorScheme='teal'
+                rightIcon={<AddUser />}
+                type='submit'
+              >
+                Agregar
+              </Button>
+            )
+        }
+      </form>
     </Box>
   )
 }
